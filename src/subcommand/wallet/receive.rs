6b0f21bbb8aa71dab3953a11d304d924c39003c0
vertbird <154328800+vertinals@users.noreply.github.com>
2024-03-06 23:34:51 +0100

    bech32

--- src/subcommand/wallet/receive.rs
+++ src/subcommand/wallet/receive.rs
@@ -7,7 +7,7 @@ pub struct Output {
 
 pub(crate) fn run(wallet: String, options: Options) -> SubcommandResult {
   let address = bitcoin_rpc_client_for_wallet_command(wallet, &options)?
-    .get_new_address(None, Some(bitcoincore_rpc::json::AddressType::Bech32m))?;
+    .get_new_address(None, Some(bitcoincore_rpc::json::AddressType::Bech32))?;
 
   Ok(Box::new(Output { address }))
 }
