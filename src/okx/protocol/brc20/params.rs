4978facfa8b215e0e30186c79889cc7158a74a74
vertbird <154328800+vertinals@users.noreply.github.com>
2024-03-06 23:33:40 +0100

    vrc20

--- src/okx/protocol/brc20/params.rs
+++ src/okx/protocol/brc20/params.rs
@@ -1,7 +1,7 @@
 use super::num::Num;
 use once_cell::sync::Lazy;
 
-pub const PROTOCOL_LITERAL: &str = "brc-20";
+pub const PROTOCOL_LITERAL: &str = "vrc-20";
 pub const MAX_DECIMAL_WIDTH: u8 = 18;
 
 pub static MAXIMUM_SUPPLY: Lazy<Num> = Lazy::new(|| Num::from(u64::MAX));
